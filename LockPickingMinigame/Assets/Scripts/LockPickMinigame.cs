98808b51a35236ccba8760342453e22f53107a3b
Mostro94 <danypaolino@gmail.com>
2022-07-26 18:20:13 +0200

    minigame prototype done!
    To do:
    clean code
    Extra
    Add animations and sounds

---
+++ LockPickingMinigame/Assets/Scripts/LockPickMinigame.cs
@@ -0,0 +1,181 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class LockPickMinigame : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private GameObject innerLock;
+    [SerializeField] private GameObject outerLock;
+    [SerializeField] private GameObject pinLock;
+    [SerializeField] private float innerLockRotationSpeed;
+    [SerializeField] private float pinSpeed;
+    [SerializeField][Min(1)][Range(1, 25)] private float unlockRange; //range for unlocking (difficulty) in degrees
+    [SerializeField] private Transform unlockPoint;
+    [SerializeField] private Transform unlockPointRanged;
+
+    private Quaternion innerLockStartRotation;
+    private Quaternion pinLockStartRotation;
+    private float maxAngle = 90f;
+    private float unlockAngle;
+    private bool canMovePin = true;
+    private bool canPlay = true;
+    private bool unlocked = false;
+    private float pinAngle; //angle between vector3.up and direction of pin
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewMinigame();
+        pinLock.GetComponent<Animator>().enabled = false;
+        innerLockStartRotation = innerLock.transform.rotation;
+        unlockPoint.transform.eulerAngles = new Vector3(unlockPoint.transform.eulerAngles.x, unlockPoint.transform.eulerAngles.y, unlockAngle);
+        unlockPointRanged.transform.eulerAngles = new Vector3(unlockPointRanged.transform.eulerAngles.x, unlockPointRanged.transform.eulerAngles.y, unlockAngle + unlockRange);
+        pinLockStartRotation = pinLock.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+        RotatePin();
+        RotateInnerLock();
+        //Debug.Log(innerLock.transform.eulerAngles.z);
+    }
+
+    private void CheckInput()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            canMovePin = false;
+            Quaternion pinRotation = pinLock.transform.rotation;
+            Debug.Log("euler: " + pinLock.transform.eulerAngles.z);
+            Debug.Log("rotation" + pinLock.transform.rotation.z);
+            Debug.Log("pinRotation" + pinRotation.z);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            canMovePin = true;
+            if (!unlocked)
+                innerLock.transform.rotation = innerLockStartRotation;
+        }
+    }
+
+    private void RotateInnerLock()
+    {
+        if (!canMovePin && canPlay)
+        {
+            float pinZ = pinLock.transform.localEulerAngles.z;
+            float innerLockZ = innerLock.transform.localEulerAngles.z;
+            pinZ = pinZ > 180 ? pinZ - 360 : pinZ;
+            innerLockZ = innerLockZ > 180 ? innerLockZ - 360 : innerLockZ;
+            Debug.Log("pinZ " + pinZ);
+            Debug.Log("pinZ " + innerLockZ);
+            if ((Mathf.Abs(unlockAngle - pinZ) < unlockRange))
+            {
+                if(innerLockZ < maxAngle && innerLockZ > -maxAngle)
+                {
+                    Debug.Log("Unlocking...");
+                    innerLock.transform.Rotate(0f, 0f, -innerLockRotationSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    unlocked = true;
+                    StartCoroutine(ResetMinigame());
+                    
+                }
+                
+            }
+
+            //else
+            //{
+            //    StartCoroutine(ShakePinLock());
+            //}
+
+        }
+    }
+
+    private void RotatePin()
+    {
+        if(canMovePin && canPlay)
+        {
+            Vector3 worldPosition;
+            Vector3 mousePos = Input.mousePosition;
+            worldPosition = cam.ScreenToWorldPoint(mousePos);
+            worldPosition.z = pinLock.transform.position.z;
+            Vector2 dir = (worldPosition - pinLock.transform.position).normalized;
+
+            pinAngle = Vector3.Angle(dir, Vector3.up);
+            pinAngle = Mathf.Clamp(pinAngle, -maxAngle, maxAngle);
+
+            float angle = Mathf.Atan2(dir.x, dir.y) * Mathf.Rad2Deg;
+            angle = Mathf.Clamp(angle, -maxAngle, maxAngle);
+            Quaternion rotation = Quaternion.AngleAxis(-angle, Vector3.forward);
+
+            pinLock.transform.rotation = Quaternion.Slerp(pinLock.transform.rotation, rotation, pinSpeed * Time.deltaTime);
+        }
+
+    }
+
+    private void NewMinigame()
+    {
+        unlocked = false;
+        unlockAngle = Random.Range(-maxAngle, maxAngle);
+        Debug.Log(unlockAngle);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 worldPosition;
+        Vector3 mousePos = Input.mousePosition;
+        worldPosition = cam.ScreenToWorldPoint(mousePos);
+        worldPosition.z = pinLock.transform.position.z;
+        Vector3 dir = (worldPosition - pinLock.transform.position).normalized;
+
+
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(pinLock.transform.position, dir * 10f);
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(pinLock.transform.position, pinLock.transform.up * 5f);
+        Gizmos.DrawWireSphere(worldPosition, 1f);
+        Handles.DrawWireArc(outerLock.transform.position, -outerLock.transform.forward, -outerLock.transform.right, 180f, 3f);
+
+    }
+
+    //private void SmoothUnlockRotation()
+    //{
+    //    
+    //}
+
+    private IEnumerator ResetMinigame()
+    {
+        canPlay = false;
+        NewMinigame();
+        unlockPoint.gameObject.SetActive(false);
+        unlockPointRanged.gameObject.SetActive(false);
+        unlockPoint.transform.eulerAngles = new Vector3(unlockPoint.transform.eulerAngles.x, unlockPoint.transform.eulerAngles.y, unlockAngle);
+        unlockPointRanged.transform.eulerAngles = new Vector3(unlockPointRanged.transform.eulerAngles.x, unlockPointRanged.transform.eulerAngles.y, unlockAngle + unlockRange);
+        yield return new WaitForSeconds(1f);
+        Debug.Log("Reset minigame");
+        innerLock.transform.rotation = innerLockStartRotation;
+        pinLock.transform.rotation = pinLockStartRotation;     
+        yield return new WaitForSeconds(0.3f);
+        unlockPoint.gameObject.SetActive(true);
+        unlockPointRanged.gameObject.SetActive(true);
+        canPlay = true;
+    }
+
+    private IEnumerator ShakePinLock()
+    {
+        pinLock.GetComponent<Animator>().enabled = true;
+        pinLock.GetComponent<Animator>().Play("pinlockShakeAnim");
+        yield return new WaitForSeconds(0.5f);
+        pinLock.GetComponent<Animator>().enabled = false;
+    }
+
+}
